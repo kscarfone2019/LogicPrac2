6d1c949ad6b4106ea3ce777f3ea30033e3e50461
ks233 <ks233@pc2-060-l.cs.st-andrews.ac.uk>
2017-03-30 16:04:43 +0100

    working on ttable

--- ttable.c
+++ ttable.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <math.h>
+#include <stdlib.h>
 #include "stack.h"
 //false = 0
 //true = 1
 //need array to hold returned values
-void evaluateAnd(int n, char operand);
+int *evaluate(int n, char operand);
 void printTable(char a, char b, char c, char d);
 int power(int base, int power);
+void makeHeader(int n);
 //arguments for main: int varSize, char formula
 int main() {
     stack workStack;
-    stackInit(&workStack, 4);
+    stackInit(&workStack, 6);
 
 
-    int arg1 = 2;
-    char arg2[] = "ab&cd|";
+    int arg1 = 3;
+    char arg2[] = "ab&c|";
     //take in equation and add each element to stack
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 6; i++) {
         if(isalpha(arg2[i])) {
             stackPush(&workStack, arg2[i]);
         }
         else if(arg2[i] == '&'){
-            char temp1 = stackPop(&workStack);
-            char temp2 = stackPop(&workStack);
+            char a = stackPop(&workStack);
+            char b = stackPop(&workStack);
             // for loop to change between 0 and 1
-            evaluateAnd(arg1, '&');
+            int *firstTwo = evaluate(arg1, '&');
 
         }
 
@@ -33,7 +35,30 @@ int main() {
 
     return 1;
 }
-void evaluateAnd(int n, char operand) {
+int *evaluate(int n, char operand) {
+
+    makeHeader(n);
+    int result, nVal;
+    //int n = 2;
+    //((2^n)-1) for i<=3 instead make var or something
+    int rows = power(2, n);
+    //int results[rows];
+    int *results = malloc(rows);
+
+    for(int i=0;i<=(rows-1);i++) {
+        result=1;
+        for(int j=n-1;j>=0;j--) {
+            nVal=i>>j; //assigns 1 or 0 to result depeing if there equal or not (*bitwise right shift)
+            //printf(" %d ",nVal & 1);
+            result = result &(nVal & 1);
+
+        }
+    //printf(" :   %d\n", result);
+    results[i] = result;
+    return results;
+    }
+}
+void makeHeader(int n) {
     //portion of code for printing header of table
     char alpha[] = "abcdefghijklmnopqrstuvwxyz";
     for (int x = 0; x < n; x++) {
@@ -47,41 +72,8 @@ void evaluateAnd(int n, char operand) {
         printf("-");
     }
     printf("\n");
-
-
-
-
-    int result, nVal;
-    //int n = 2;
-    //((2^n)-1) for i<=3 instead make var or something
-    int rows = power(2, n);
-
-    for(int i=0;i<=(rows-1);i++) {
-        //result=1;
-        char test = '&';
-
-        for(int j=n-1;j>=0;j--) {
-            //printf("i: %i J: %i\n", i, j);
-            nVal=i>>j; //assigns 1 or 0 to result depeing if there equal or not
-            printf(" %d ",nVal & 1);
-
-            switch (operand) {
-                case '&':
-                result = 1;
-                result = result &(nVal & 1);
-                case '^':
-                result = 0;
-                result = result ^(nVal & 1);
-            }
-
-
-             //line that changes between and or/ so on
-            //printf("%i", j);
-            //result = result & (nVal & 1);
-        }
-    printf(" :   %d\n", result);
-    }
 }
+
 int power(int base, int power) {
     int result = 1;
     for (int i = 0; i < power; i++) {
